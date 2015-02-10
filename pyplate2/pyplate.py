a77d2af021fe6af4b87447f17e7978c7678e590f
ThatITNinja <ThatITNinja@hotmail.com>
2015-02-10 12:05:58 -0500

    Lots of work to how templates are extracted.

--- pyplate2/pyplate.py
+++ pyplate2/pyplate.py
@@ -1,5 +1,9 @@
 import struct
-from collections import ordereddict
+from collections import OrderedDict
+
+#debugging
+from pprint import PrettyPrinter
+pp = PrettyPrinter().pprint
 
 
 #used when unpacking during an extraction
@@ -20,12 +24,11 @@ FLOAT, _FLOAT_SIZE = "f", struct.calcsize("f")
 DOUBLE, _DOUBLE_SIZE = "d", struct.calcsize("d")
 
 
-def template(name, **kwargs):
+def template(**kwargs):
     template = {}
-    template["name"] = name
     template["type"] = "template"
-    template["members"] = ordereddict()
-    template["member_values"]
+    template["members"] = OrderedDict()
+    template["member_values"] = OrderedDict()
     for member in kwargs:
         template["members"][member] = kwargs[member]
     return template
@@ -48,20 +51,60 @@ def _search_for_member_value(template, member_values, member_name):
 
 def extract_string_template(template, string, context=None):
     unpack_string = ""
+    unpack_member_sequence = []
     unpack_sequence_prepped = False
     for member in template["members"]:
-        if isinstance(member, str):
+        if isinstance(template["members"][member], str):
+            #flag the sequence as ready to be extracted, since it is if need be.
+            unpack_sequence_prepped = True
             unpack_string += template["members"][member]
-        elif isinstance(member, tuple):
-            member_length_value = _search_for_member_value(template, member[1])
-            unpack_string += "%s%s" % (member_length_value, member[0])
-        elif isinstance(member, dict):
+            unpack_member_sequence.append(member)
+
+        elif isinstance(template["members"][member], tuple):
+            #
+            #
+            # COME BACK TO THIS. HOW SHOULD DEFAULT VALUES BE HANDLED? SKIPPED DURING EXTRACT?
+            # OR SHOULD THEY BE EXTRACTED BY LENGTH?
+            #
+            #flag the sequence as ready to be extracted, since it is if need be.
+            unpack_sequence_prepped = True
+            if isinstance(template["members"][member][1], str):
+                #self referenced value support
+                member_length_value = _search_for_member_value(template, template["member_values"], template["members"][member][1])
+            elif isinstance(template["members"][member][1], int) or isinstance(template["members"][member][1], float):
+                #default value support
+                member_length_value = template["members"][member][1]
+                #add value to member values
+                unpack_string += "%s%s" % (member_length_value, member[0])
+                unpack_member_sequence.append(member)
+
+        elif isinstance(template["members"][member], dict):
             #extract the templates member values now, we are about to extract
-            #the child
+            #a child template
             if unpack_sequence_prepped:
-                struct.unpack(unpack_string, )
+                #get the string to the propper size to extract and unpack
+                unpacked_values = struct.unpack(unpack_string, string[0:struct.calcsize(unpack_string)])
+                for index, val_member in enumerate(unpack_member_sequence):
+                    template["member_values"][val_member] = unpacked_values[index]
+                #reset to default, we just extracted everything we could.
+                unpack_sequence_prepped = False
+                unpack_string = ""
+                unpack_member_sequence = []
+            #extract the template, we have our context now.
+            template["member_values"][member] = extract_string_template(template["members"][member], string, template)
+
         else:
+            #we cant parse this type. oops. freakout.
             raise TypeError
+
+    #parse the string if it hasn't been parsed yet.
+    if unpack_sequence_prepped:
+        unpacked_values = struct.unpack(unpack_string, string[0:struct.calcsize(unpack_string)])
+        for index, val_member in enumerate(unpack_member_sequence):
+            template["member_values"][val_member] = unpacked_values[index]
+    #everything is unpacked and stored, return it.
+    return template
+
 
 
 
@@ -70,14 +113,14 @@ def extract_string_template(template, string, context=None):
 def extract_file_template(templat, fobj, context=None):
     pass
 
-if __name__ = "__main__":
-    my_temp = template(
-        my_int = INT,
-        my_char = CHAR,
-        my_double = DOUBLE,
-        my_inner_template = template(
-            my_int2 = INT,
-            my_dependant_value = (CHAR, 'outer_test.my_int'),
-            my_other_dep_value = (CHAR, 'outer_test.inner_test.my_int2')
+if __name__ == "__main__":
+    my_template = template(
+        test=INT,
+        inner_template=template(
+            test2=INT,
+            test3=(INT,'test')
         )
+
     )
+    my_value = extract_string_template(my_template, "12341234")
+    pp(my_value)
